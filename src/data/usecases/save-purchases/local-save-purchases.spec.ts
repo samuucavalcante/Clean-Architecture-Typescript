22621c32c2b404b8eb92a549aa4c8abd5dc8f979
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2021-12-25 17:56:58 -0300

    move production code to there own files

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -1,13 +1,5 @@
-class LocalSavePurchases {
-  constructor(private readonly cacheStore: CacheStore) {}
-  async save(): Promise<void> {
-    this.cacheStore.delete('purchases');
-  }
-}
-
-interface CacheStore {
-  delete(key: string): void;
-}
+import { CacheStore } from '../../protocols/cache';
+import { LocalSavePurchases } from './local-save-purchases';
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
