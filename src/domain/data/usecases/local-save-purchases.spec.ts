4de0e922a4e69265ee59c5c7cd3feed16d1f6ec1
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2021-12-25 17:38:55 -0300

    ensure LocalSavePurchases delete old cache on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,13 +1,20 @@
 class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) {}
+  async  save(): Promise<void> {
+    this.cacheStore.delete()
+  }
 }
 
 interface CacheStore {
-
+  delete(): void;
 }
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
+
+  delete(): void {
+    this.deleteCallsCount++
+  } 
 }
 
 describe("LocalSavePurchases", () => {
@@ -16,5 +23,13 @@ describe("LocalSavePurchases", () => {
     new LocalSavePurchases(cacheStore);
 
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test("Should delete old cache on sut.save", async () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+    await sut.save()
+
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
