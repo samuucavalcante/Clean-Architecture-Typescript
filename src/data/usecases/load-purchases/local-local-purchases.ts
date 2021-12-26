7f39fb8727365707f6a483c771ef3da52796e78b
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2021-12-26 14:04:23 -0300

    ensure LocalLoadPurchases returns an empty list if cache is then 3 days old

--- src/data/usecases/load-purchases/local-local-purchases.ts
+++ src/data/usecases/load-purchases/local-local-purchases.ts
@@ -19,7 +19,11 @@ export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
   async loadAll(): Promise<Array<LoadPurchases.Result>>{
     try {
       const cache = this.cacheStore.fetch(this.key);
-      return  cache.value;
+      const maxAge = new Date(cache.timestamp)
+      maxAge.setDate(maxAge.getDate() + 3)
+      if(maxAge > this.currentDate) return cache.value;
+      else this.cacheStore.delete(this.key);
+      return [];
     } catch (err) {
       this.cacheStore.delete(this.key);
       return [];
