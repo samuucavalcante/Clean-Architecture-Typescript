24c285c0741d6117f63f26f9349e803e385693df
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2021-12-25 23:41:55 -0300

    simplify CacheStorage insertion with a replace method

--- src/data/protocols/cache/cache-store.ts
+++ src/data/protocols/cache/cache-store.ts
@@ -2,4 +2,5 @@ import { SavePurchases } from "../../../domain/usecases/save-purshases";
 export interface CacheStore {
   delete(key: string): void;
   insert(key: string, value: any): void;
+  replace(key: string, value: any): void;
 }
--- src/data/usecases/save-purchases/local-save-purchases.ts
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -7,8 +7,7 @@ export class LocalSavePurchases implements SavePurchases {
     private readonly timestamp: Date
   ) {}
   async save(purchases: Array<SavePurchases.Params>): Promise<void> {
-    this.cacheStore.delete("purchases");
-    this.cacheStore.insert("purchases", {
+    this.cacheStore.replace("purchases", {
       timestamp: this.timestamp,
       value: purchases
     });
